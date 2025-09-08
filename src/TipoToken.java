56a947d528f3f666707578e5894bc6656b7f9436
AGK7_acerV15 <lokingtseries@gmail.com>
2025-09-07 22:04:25 -0300

    Interfaz Grafica añadida

--- src/TipoToken.java
+++ src/TipoToken.java
@@ -1,5 +1,5 @@
 //Por buena practica creamos una enumeracion (enum) para los tipos de token
-//Porque? porque son un conjunto fijo de valores
+//Porque? porque son un conjunto fijo de valores (CONSTANTES)
 public enum TipoToken {
     PALABRA_CLAVE, IDENTIFICADOR, NUMERO, OPERADOR, DELIMITADOR, ERROR
 }
--- src/Token.java
+++ src/Token.java
@@ -4,7 +4,8 @@ public class Token {
     private final TipoToken tipo;
     private final String valor;
 
-    public Token(TipoToken tipo, String valor){
+    //este es el contructor privado
+    private Token(TipoToken tipo, String valor){
         this.tipo=tipo;
         this.valor=valor;
     }
@@ -15,5 +16,27 @@ public class Token {
         //Va imprimir los atributos del token en el formato q pide el profe:
         // <TIPO_DE_TOKEN> -> <VALOR_DEL_TOKEN>
         return String.format("<%s> -> <%s>", tipo, valor);
+    }
+
+    public static class Builder {
+        private TipoToken tipo;
+        private String valor;
+
+        // Métodos para "construir" el token paso a paso
+        // Devuelven el propio Builder para poder encadenar llamadas
+        public Builder conTipo(TipoToken tipo) {
+            this.tipo = tipo;
+            return this;
+        }
+
+        public Builder conValor(String valor) {
+            this.valor = valor;
+            return this;
+        }
+
+        // El metod final que crea el objeto Token
+        public Token construir() {
+            return new Token(this.tipo, this.valor);
+        }
     }
 }
